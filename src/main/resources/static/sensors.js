7d7a936f546ad90efd5ef0e94097ec7f4a8b2451
haikuai <haikuai0070@gmail.com>
2023-05-13 11:01:34 -0400

    add moisture by time chart to UI

--- src/main/resources/static/sensors.js
+++ src/main/resources/static/sensors.js
@@ -1,28 +1,34 @@
+let ctx = canvas.getContext('2d');
+let labels = [];
+let data = [];
+let config = {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Graph Line',
+            data: data,
+            backgroundColor: 'rgba(0, 119, 204, 0.3)'
+        }]
+    }
+};
+let measurements_plot = new Chart(ctx, config);
+
 function onSelectedChange() {
     let selected = document.getElementById("selected").value;
     document.getElementById("measurements_iframe").src = "http://192.168.1.80:8080/measurements/" + selected;
 
     let jsonList = document.getElementById("measurements_iframe").innerHTML;
+    console.log(jsonList);
     let labels = jsonList.jsonarray.map(function (e) {
         return e.timeStamp;
     });
     let data = jsonList.jsonarray.map(function (e) {
         return e.moisture;
     });
+    console.log(labels);
+    console.log(data);
 
-    let ctx = canvas.getContext('2d');
-    let config = {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Graph Line',
-                data: data,
-                backgroundColor: 'rgba(0, 119, 204, 0.3)'
-            }]
-        }
-    };
-
-    let measurements_plot = new Chart(ctx, config);
-
+    measurements_plot.labels = labels;
+    measurements_plot.data = data;
 }
