493d4a2ebde3cdddc573c0ab43f442733c1266ad
haikuai <haikuai0070@gmail.com>
2023-05-16 10:10:40 -0400

    fix chart

--- src/main/resources/static/sensors.js
+++ src/main/resources/static/sensors.js
@@ -1,31 +1,29 @@
-
-    let ctx = document.getElementById("measurements_plot").getContext("2d");
-    let labels = [];
-    let data = [];
-    let config = {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Graph Line',
-                data: data,
-                backgroundColor: 'rgba(0, 119, 204, 0.3)'
-            }]
-        }
-    };
-    window.measurements_plot = new Chart(ctx, config);
-
+let ctx = document.getElementById("measurements_plot").getContext("2d");
+let labels = [];
+let data = [];
+let config = {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Graph Line',
+            data: data,
+            backgroundColor: 'rgba(0, 119, 204, 0.3)'
+        }]
+    }
+};
+window.measurements_plot = new Chart(ctx, config);
 
 function onSelectedChange() {
     let selected = document.getElementById("selected").value;
-    document.getElementById("measurements_iframe").src = "http://192.168.1.80:8080/measurements/" + selected;
+    document.getElementById("measurements_div").src = "http://192.168.1.80:8080/measurements/" + selected;
 
-    let jsonList = document.getElementById("measurements_iframe").innerHTML;
+    let jsonObject = JSON.parse(document.getElementById("measurements_div").innerHTML);
 
-    let labels = jsonList.jsonarray.map(function (e) {
+    let labels = jsonObject.map(function (e) {
         return e.timeStamp;
     });
-    let data = jsonList.jsonarray.map(function (e) {
+    let data = jsonObject.map(function (e) {
         return e.moisture;
     });
 
